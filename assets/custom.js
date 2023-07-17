2897d027f56f10f967edb469fc99b25f387cebaf
xiaofeng900 <guo.xiaofeng900@hotmail.com>
2023-07-17 15:30:21 -0400

    feat: 🎸 rebuilt resources section on product page

--- assets/custom.js
+++ assets/custom.js
@@ -7,28 +7,6 @@ $(document).ready(function () {
         autoplay: false,
     });
 
-    $('.resource-slider').slick({
-        loop: true,
-        dots: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
-    });
-
     if ($('.quotes-wrapper').length) {
         $('.quotes-wrapper').each(function () {
             const quotesSlider = $('.quotes-slider')
